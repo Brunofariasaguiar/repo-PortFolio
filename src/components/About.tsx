8597218da2369398c505b7bf489c73fd3e7ce7d5
Brunofariasaguiar <117117799+Brunofariasaguiar@users.noreply.github.com>
2023-07-05 14:20:27 -0400

    Add files via upload

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -106,7 +106,7 @@ export function About() {
               </tbody>
             </table>
           </div>
-          <div className="absolute -left-2 -top-4 h-24 w-20 rounded-lg bg-gray-600 bg-[url(images/eu2.jpg)] bg-cover bg-center md:-left-12 md:-top-12 md:h-72 md:w-56"></div>
+          <div className="absolute -left-2 -top-4 h-24 w-20 rounded-lg bg-gray-600 bg-[url(https://lh3.googleusercontent.com/pw/AJFCJaXX8_JCpG0Bdl1UlHHHTuwqREYfIw3hLFG6pqo1kOEnKdqBNeritrRLPSx7D4JddM2YldrwHawI0Szg0iZagELYztQ1VSJtI3_J46QUb4LqQS9PognYL20ZT63W74JUs5EBigYlaN4MIKkwoSCOixClNQvP5PFwTwQ8HfrkZX1jtfL2ztQalQI4a7Zv7bja0ZqsmfjzlJw1wiXo0OmtHSRP8ZNkquBQC-68ZQWpudyBHpUgFrA9ABskHIJJmELueYUpbTMikwA_ShHD_GYkXuetLuiVI0bqT7RMmwG4JkFLU1MQAG84cBYr-yEv9yxMzgKkP3k2ir0837_NMyhixqKrU7-wBqoid3MkAgNglpqNgb9GmtbF5aouZj9J2MPE9_Rehuys6NE_Zw9fKMtgiOsL34_mEv9ftKvkChoHgfckG75xqGaG1RNMTM2mTCmLmJ28gYb1DirwauZ1rwNn4fy58fzUJzvz9UH-aCQpiB75tIDEZOefeXfStYDYrfNiPaKbF0qgOHCKazJwMb8ny--CF499cLE78Wb-rTxQ-9SVm_WXcd5Grlmfk8yOytgLxqIENuZXONWDlkwUTlJwOSKGIiBWj7D11VoL1dsuTy3wnYNh_pzsCT0buRva12hllV5zlAJTHkeMaWJHJJxqqQpNM9vCb_hcWjpZseVbjsQytoZ_3yzGIDjS7vBKxn0YphiqYsreiPvsd1XcgE8XsZSGmn3zChol84_jicJcoYq_Whh-deUu56mOrgrxS9I2YtPdT-nd986-Kwp16nzjLUm-us9d5r5PJGwvT9-0uPYq7g5wf4SWmyCWGg54CisQK_vyfsjgP1E2a_WmhmbbLxVt68ytE7Hk29tUE2_5W_n_TYAe2RSljDNxqXqsOtFMhWlj_ff9AnYe7z486sxBCg=w671-h720-s-no?authuser=0)] bg-cover bg-center md:-left-12 md:-top-12 md:h-72 md:w-56"></div>
         </div>
       </div>
     </section>
